9f886c9d95a6d452d5b301d97ee758d6328f0c35
Juli Kumari <juli_kumari@persistent.com>
2023-02-24 11:27:39 +0530

    normal click

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("Hello world")
+// prompt("Hi This is banana")
+
+// var userName = prompt("Hi give your name.")
+// alert("My name is "+ userName )
+
+var button = document.querySelector("#btn");
+
+ button.addEventListener("click", function (){
+ console.log("Clicked!");
+}
+)
