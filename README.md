226500c7760cb82d0ea3e830b162205d88fd27d7
Juli Kumari <juli_kumari@persistent.com>
2023-02-24 14:38:34 +0530

    snnipet  ready

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Banana Tranlater App
 
 A banana language translator app to understand minions.
+
+Convert from English to Minion speak. 
+
+This language is spoken by the minions in Despicable Me.
--- app.js
+++ app.js
@@ -4,17 +4,38 @@
 // var userName = prompt("Hi give your name.")
 // alert("My name is "+ userName )
 
-
 var textArea = document.querySelector("#text-input");
 
 var button = document.querySelector("#btn");
 
-var output = document.querySelector("#output")
+var output = document.querySelector("#output");
 
-function clickEvent(){
-   output.innerText = "shdjshfjshfise "+ textArea.value;
-   }
+var serverURL = "https://api.funtranslations.com/translate/minion.json"; // API
 
- button.addEventListener("click", clickEvent); //  callback function
+function getTranslationURL(text) {
+  return serverURL + "?"+ "text=" + text;
+}
 
+function errorHandling(error) {
+  console.log("Error occure " + error);
+  alert("Server is not responding........Wait for sometime.");
+}
 
+function clickEvent() {
+  //    output.innerText = "shdjshfjshfise "+ textArea.value;
+
+  //taking input
+  var inputText = textArea.value;
+
+  // calling server // processing done on server
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    // .then((value) => console.log(value.contents.translated))
+    .then(jsonResponse => {
+        var jsonRes= jsonResponse.contents.translated;
+        output.innerText = jsonRes;
+    })
+    .catch(errorHandling); // catch error here
+}
+
+button.addEventListener("click", clickEvent); //  callback function
