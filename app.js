384c808f55ed969d3d998ca0f6efd2cd908f8c67
Juli Kumari <juli_kumari@persistent.com>
2023-02-24 11:54:28 +0530

    get text area value

--- app.js
+++ app.js
@@ -4,8 +4,16 @@
 // var userName = prompt("Hi give your name.")
 // alert("My name is "+ userName )
 
+
+var textArea = document.querySelector("#text-input");
+
 var button = document.querySelector("#btn");
+
 function clickEvent(){
     console.log("Clicked!");
+    console.log(textArea.value);
    }
+   
  button.addEventListener("click", clickEvent); //  callback function
+
+
